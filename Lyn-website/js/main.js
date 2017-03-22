f0296c7030feb9c646e81d48096abe4efed21ca0
Wilfred Ruck <wilfredoruck@gmail.com>
2017-03-22 17:09:38 -0400

    added javascript file to put all scripts in and clean code

---
+++ Lyn-website/js/main.js
@@ -0,0 +1,46 @@
+$(function() {
+	var pull = $('#pull');
+	var	menu = $('nav ul');
+
+	$(pull).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	
+	$(window).resize(function(){
+		var w = $(this).width();
+
+		if(w > 800 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+
+	$('li').on('click', function(e) {		
+		var w = $(window).width();
+		if(w < 800 ) {
+			menu.slideToggle();
+		}
+	});
+
+	$('.panel').height($(window).height());
+	
+});
+
+$(function() {
+	var addToAll = false;
+	var gallery = true;
+	var titlePosition = 'inside';
+	$(addToAll ? 'img' : 'img.fancybox').each(function(){
+		var $this = $(this);
+		var title = $this.attr('title');
+		var src = $this.attr('data-big') || $this.attr('src');
+		var a = $('<a href="#" class="fancybox"></a>').attr('href', src).attr('title', title);
+		$this.wrap(a);
+	});
+	if (gallery)
+		$('a.fancybox').attr('rel', 'fancyboxgallery');
+	$('a.fancybox').fancybox({
+		titlePosition: titlePosition
+	});
+});
+$.noConflict();
