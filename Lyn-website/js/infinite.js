90f951f898d07841cb2205d156b60ad4fab9d50d
Wilfred Ruck <wilfredoruck@gmail.com>
2017-03-29 01:09:39 -0400

    testing out lazyload

---
+++ Lyn-website/js/infinite.js
@@ -0,0 +1,15 @@
+ $(".images div").slice(5).hide();
+
+ var mincount = 5;
+ var maxcount = 20;
+
+
+ $(window).scroll(function () {
+     if ($(window).scrollTop() + $(window).height() >= $(document).height() - 5) {
+         $(".images div").slice(mincount, maxcount).slideDown(100);
+
+         mincount = mincount + 10;
+         maxcount = maxcount + 10
+
+     }
+ });
