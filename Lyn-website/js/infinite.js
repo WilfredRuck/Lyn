899d1cd842521836f015c9728bd0944447f2d150
Wilfred Ruck <wilfredoruck@gmail.com>
2017-03-31 21:50:14 -0400

    delete extra spacin

--- Lyn-website/js/infinite.js
+++ Lyn-website/js/infinite.js
@@ -3,13 +3,11 @@
  var mincount = 5;
  var maxcount = 20;
 
-
  $(window).scroll(function () {
      if ($(window).scrollTop() + $(window).height() >= $(document).height() - 5) {
          $(".images div").slice(mincount, maxcount).slideDown(100);
 
          mincount = mincount + 10;
          maxcount = maxcount + 10
-
      }
  });
